3e52db1d8f2078e61141c02ffd2f16592260b630
So-ooming <tnals6254@naver.com>
2023-06-29 00:19:29 +0900

    [Create]: DeliveryTable 추가

--- Assets/3.Script/Tables/DeliveryTable.cs
+++ Assets/3.Script/Tables/DeliveryTable.cs
@@ -2,17 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeliveryTable : MonoBehaviour
+public class DeliveryTable : Table
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        fire = this.transform.Find("FX_Fire_Big_01").GetComponent<Fire>();
     }
 }
