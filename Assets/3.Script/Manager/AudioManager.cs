cc669a864fa1f75b144985f3314b1cc6eec5670f
Kim-hyun-hee <asdfzxcv0730@naver.com>
2023-07-10 10:59:09 +0900

    [Feat]: PausePanel 애니메이션 완료, control_bg 이미지 깨진거 수정 필요

--- Assets/3.Script/Manager/AudioManager.cs
+++ Assets/3.Script/Manager/AudioManager.cs
@@ -1,8 +1,91 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.Audio;
 using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    public string name; // ∞Ó ¿Ã∏ß
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+
+    [Range(.1f, 3f)]
+    public float pitch;
+    public bool loop;
+
+    [HideInInspector]
+    public AudioSource source;
+}
+
 
 public class AudioManager : MonoBehaviour
 {
-    
+    public Sound[] sounds;
+    private bool underOneMinute = false;
+
+    void Awake()
+    {
+        foreach (Sound sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+            sound.source.loop = sound.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Restaurant");
+    }
+
+    void Update()
+    {
+        //if (OrderManager.Instance.levelTime <= 60 && !underOneMinute)
+        //{
+        //    sounds[0].pitch = 1.5f;
+        //    underOneMinute = true;
+        //}
+    }
+
+    public void Play(string name)
+    {
+        foreach (Sound sound in sounds)
+        {
+            if (sound.name == name)
+            {
+                sound.source.Play();
+                break;
+            }
+
+        }
+    }
+
+    public void Stop(string name)
+    {
+        foreach (Sound sound in sounds)
+        {
+            if (sound.name == name)
+            {
+                sound.source.Stop();
+                break;
+            }
+
+        }
+    }
+
+    public bool IsPlaying(string name)
+    {
+        foreach (Sound sound in sounds)
+        {
+            if (sound.name == name)
+                return sound.source.isPlaying;
+        }
+        return false;
+    }
+
+
 }
+
