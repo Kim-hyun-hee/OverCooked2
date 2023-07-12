09d605a2672144830fe01c9e6913ed261ca33505
Kim-hyun-hee <asdfzxcv0730@naver.com>
2023-07-12 18:23:29 +0900

    .

--- Assets/3.Script/Etc/DropEvent.cs
+++ Assets/3.Script/Etc/DropEvent.cs
@@ -1,17 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class DropEvent : MonoBehaviour, IPointerEnterHandler//, IPointerExitHandler
 {
     [SerializeField] private GameObject bg;
     [SerializeField] private List<GameObject> otherBg = new List<GameObject>();
-    public Transform defaultTransform;
+    public List<Transform> defaultTransforms = new List<Transform>();
 
     private void Start()
     {
-        defaultTransform = this.transform;
+        for (int i = 0; i < otherBg.Count; i++)
+        {
+            defaultTransforms.Add(otherBg[i].transform);
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
@@ -19,7 +23,7 @@ public class DropEvent : MonoBehaviour, IPointerEnterHandler//, IPointerExitHand
         UIManager.Instance.DropDown(bg, -60, 0.5f);
         for (int i = 0; i < otherBg.Count; i++)
         {
-            UIManager.Instance.MoveDefaultPos(otherBg[i], defaultTransform, 0.01f);
+            UIManager.Instance.MoveDefaultPos(otherBg[i], defaultTransforms[i], 0.01f);
             otherBg[i].SetActive(false);
         }
     }
